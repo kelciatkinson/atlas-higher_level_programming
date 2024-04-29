96802554dae2dbcf87d7322e96ccc68cc41e1f23
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-29 22:33:24 +0000

    printing all combos of 2 digits with no repeating digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+for i in range(90):
+    if (i / 10 % 10 > i % 10):
+        pass
+    elif (i == 90 - 1):
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
