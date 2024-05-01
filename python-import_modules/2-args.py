2b672b1ecd198139d67208b937878447b2c438b2
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-01 16:28:58 +0000

    finalized function to print number of arguments, working

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,12 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1 or len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv[0])))
-        for i in sys.argv:
-            print("{}: {}".format(len(sys.argv), i))
+    number_of_arg = len(sys.argv) - 1
+    if number_of_arg == 1:
+        print("{} argument:".format(number_of_arg))
+        print("{}: {}".format((number_of_arg), sys.argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv)))
-        for i in sys.argv:
-            print("{}: {}".format(len(sys.argv), i))
+        print("{} arguments:".format(number_of_arg))
+        for i in range(len(sys.argv)):
+            if i > 0:
+                print("{}: {}".format((i), sys.argv[i]))
