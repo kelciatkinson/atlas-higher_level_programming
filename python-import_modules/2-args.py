93ec20488f528fa3bea167b778b5a03eada9080a
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-01 15:11:35 +0000

    trying else part of conditional

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,5 +5,9 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("{} argument:".format(len(sys.argv)))
-        for i in (sys.argv):
+        for i in sys.argv:
+            print("{}: {}".format(len(sys.argv), i))
+    else:
+        print("{} arguments:".format(len(sys.argv)))
+        for i in sys.argv:
             print("{}: {}".format(len(sys.argv), i))
