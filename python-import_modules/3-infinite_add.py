bacf9e4f5a25bb51e70f752cd6047992d371b1a9
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-01 16:57:27 +0000

    printing result of multiple arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    number_of_arg = len(sys.argv) - 1
+    if number_of_arg == 0:
+        print(number_of_arg)
+    elif number_of_arg == 1:
+        print(sys.argv[number_of_arg])
+    else:
+        result = 0
+        for i in range(1, len(sys.argv)):
+            result = result + int(sys.argv[i])
+        print(result)
