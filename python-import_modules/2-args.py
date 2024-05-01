feb7c21310b81fc530d0e322145721a764504ab6
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-01 15:28:27 +0000

    testing indexing

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 1 or len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv[1])))
+        print("{} argument:".format(len(sys.argv[0])))
         for i in sys.argv:
             print("{}: {}".format(len(sys.argv), i))
     else:
