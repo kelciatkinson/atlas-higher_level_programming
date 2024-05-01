eccc5bdb712be21442d2704231668d5d5362576a
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-01 16:34:37 +0000

    fixed case of 0 arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 
 if __name__ == "__main__":
     number_of_arg = len(sys.argv) - 1
-    if number_of_arg == 1:
+    if number_of_arg == 0:
+        print("{} arguments:".format(number_of_arg))
+    elif number_of_arg == 1:
         print("{} argument:".format(number_of_arg))
         print("{}: {}".format((number_of_arg), sys.argv[1]))
     else:
         print("{} arguments:".format(number_of_arg))
         for i in range(len(sys.argv)):
             if i > 0:
-                print("{}: {}".format((i), sys.argv[i]))
-            else:
                 print("{}: {}".format((i), sys.argv[i]))
