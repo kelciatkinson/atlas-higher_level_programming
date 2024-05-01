623601ca74d26744aa6daf1f43aa73d19e0c8113
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-01 15:13:18 +0000

    testing checker

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 1 or len(sys.argv) == 2:
         print("{} argument:".format(len(sys.argv)))
         for i in sys.argv:
             print("{}: {}".format(len(sys.argv), i))
