fc639d360235fe34248a9549483145cf6e01a359
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-02 05:03:22 +0000

    function iterates through lists of lists and prints in proper formatting

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for list in matrix:
+        num = 1
+        length = len(list)
+
+        for i in list:
+            if num == length:
+                print("{}".format(i), end="")
+            else:
+                print("{}".format(i), end=" ")
+            num += 1
+        print()
 
