6b01a871c0254e56879cc189824d96b203d4b040
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-02 02:56:18 +0000

    function returns new_list of integers after adding and removing the element from main

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list)-1:
+        return my_list
+    else:
+        my_list.insert(idx, element)
+        new_list = my_list.pop(idx+1)
+        return new_list
