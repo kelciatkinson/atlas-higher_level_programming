2211ecabfd9535204f8c5049d0b0d99db991e3a6
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-02 04:02:30 +0000

    :d to specify integer

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     backwards_list = my_list[-1::-1]
     for i in range(len(backwards_list)):
-        print("{}".format(backwards_list[i]))
+        print("{:d}".format(backwards_list[i]))
