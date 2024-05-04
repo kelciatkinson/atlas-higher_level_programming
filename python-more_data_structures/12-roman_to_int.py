28c1487f6a0381a82f60cf66469c6d02a4ca6b60
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-04 18:56:52 +0000

    working on function

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def roman_to_int(roman_string):
+    if not roman_string or roman_string is None:
+        return 0
+    else:
+        #for num in string:
+            #if string > num:
+                #print("bigger")
+            #else:
+                #print("smaller")
+        return string
