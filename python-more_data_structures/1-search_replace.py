0d7b564777d3fced4ba65b1f9e2c195e7fb66ee8
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-04 03:44:37 +0000

    function to replace element by looping through list and comparing each integer, replacing the search int with the replace ine

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for index, int in enumerate(new_list):
+        if int == search:
+            new_list[index] = replace
+    return new_list
