f38ae74a1a4d820c9c9b5fbbf21a6a8cbe514b00
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-04 06:47:00 +0000

    fixed indentation

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     for key in a_dictionary:
-            if a_dictionary[key] > high_score:
-                best_score = key
-                high_score = a_dictionary[key]
+        if a_dictionary[key] > high_score:
+            best_score = key
+            high_score = a_dictionary[key]
     return best_score
