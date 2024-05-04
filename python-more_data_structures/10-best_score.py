04d000746d7f5a72af9f04d4d7d8df27ac514ddd
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-04 06:46:24 +0000

    fixed function to compare high score to key with highest value and save key (person) as best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_score = 0
+    high_score = 0
     if not a_dictionary:
         return None
     for key in a_dictionary:
-            if a_dictionary[key] > best_score:
-                best_score = a_dictionary[key]
+            if a_dictionary[key] > high_score:
+                best_score = key
+                high_score = a_dictionary[key]
     return best_score
