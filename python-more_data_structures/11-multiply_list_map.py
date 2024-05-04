f71967127a11c3eb85ca407020f4597c1bdbf0d3
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-04 07:01:56 +0000

    fixed function to avoid using for loop

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     new_list = my_list.copy()
-    new_list = list(map(lambda x: x * number, (i for i in new_list)))
+    new_list = list(map(lambda x: x * number, new_list))
     return new_list
