058a491c0dad78407dd7cb229ba8dfdcf282e0c0
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-04 06:53:25 +0000

    function multiplies list without loop, using map function, returns new list

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-
+def multiply_list_map(my_list=[], number=0):
+    new_list = my_list.copy()
+    new_list = list(map(lambda x: x * number, (i for i in new_list)))
+    return new_list
