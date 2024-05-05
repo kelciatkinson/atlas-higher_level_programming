adcc7cbed4442066fc604e099809c23f4cea8711
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-05 00:32:23 +0000

    try/except catches index error that the main function runs with safe_print_list, returns only the length of x or length of the list if x is out of range. function prints x number of elements in list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def safe_print_list(my_list=[], x=0):
+    length = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            length += 1
+    except:
+        pass
+    print()
+    return length
