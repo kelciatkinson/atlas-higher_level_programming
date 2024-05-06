0f117af8512109c67c40b790fb3c4ef78a173303
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-06 14:17:02 -0500

    fixed conditional to output 0/b = 0.0

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     except:
         return None
     finally:
-        if result == 0:
+        if b == 0:
             print("Inside result: None")
             return None
         print("Inside result: {:.1f}".format(result))
