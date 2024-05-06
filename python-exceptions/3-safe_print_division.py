b4496485975538535a0f520f831eb478e79b0be3
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-06 14:14:12 -0500

    function prints result and attempts division, but does not print None when the answer is 0.0

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except:
+        return None
+    finally:
+        print("Inside result: {:.1f}".format(result))
+        return result
