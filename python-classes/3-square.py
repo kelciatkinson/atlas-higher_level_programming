14ff2fb8122be3be563a73cfb4b99d74b742eaf0
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-09 01:18:38 -0500

    added and changed permission to main file, added blueprint of new file, will continue

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""defines a new class called Square"""
+
+
+class Square:
+    """defines a Square"""
+    def __init__(self, size=0):
+        self.__size = size
+        """private instance attribute size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
