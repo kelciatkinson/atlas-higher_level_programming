ec301247cea108a2ea4c39a5b4b6a7da82cd7bd2
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-09 01:08:53 -0500

    added private instance attribute of size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""defines a new class called Square"""
+
+
+class Square:
+    """defines a Square and has private
+    instance attribute size"""
+    def __init__(self, size):
+        self.__size = size
