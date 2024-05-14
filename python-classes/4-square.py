b3bcf8faa0af736acfc94d2da751f9e77f7b0d04
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 10:45:48 -0500

    fixed setter to check if value is non negative integer, then sets size attribute to the value

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,22 +5,28 @@
 class Square:
     """defines a Square"""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
         """private instance attribute size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
-    def area(self):
-        """returns the public instance method area,
-        that is the current square area"""
-        return self.__size * self.__size
-
     @property
     def size(self):
+        """accesses and returns the size attribute"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """sets the size attribute to the value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """returns the public instance method area,
+        that is the current square area"""
+        return self.__size * self.__size
