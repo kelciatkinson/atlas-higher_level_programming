fecebeeabce78e76388c860d822f5e74901a4e94
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 18:59:47 -0500

    trying case for negative integers

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -90,4 +90,5 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """returns a new Rectangle instance with width == height == size"""
-        return cls(size, size)
+        if size >= 0:
+            return cls(size, size)
