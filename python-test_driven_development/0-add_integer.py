156780b0972346926b312ada3955109b149e8d83
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 14:29:38 -0500

    added documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""this module contains addition function"""
+
+
 def add_integer(a, b=98):
-    """addition function"""
+    """addition function for two numbers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
