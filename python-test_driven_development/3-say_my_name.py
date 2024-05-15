221c2c42cc6954a720ff827f15638accf8abe4f6
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 21:06:16 -0500

    added newline at EOF, added conditional for last name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     """prints a string"""
-    if type(first_name) is str:
+    if type(first_name) is str and type(last_name) is str:
         print("My name is {} {}".format(first_name, last_name))
     else:
         raise Exception("first_name must be a string")
+    
