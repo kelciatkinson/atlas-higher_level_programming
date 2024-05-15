f8c1c861e350851ef005a8d2034f0652f8be19d9
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 21:10:27 -0500

    fixed function to raise error if last name is not a string

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     """prints a string"""
-    if type(first_name) is str and type(last_name) is str:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        raise Exception("first_name must be a string")
-    
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
