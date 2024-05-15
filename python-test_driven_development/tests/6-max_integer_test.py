0175867c2c90f7a38724cf389d41955507f832c5
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 22:17:25 -0500

    added test to test file

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+"""unittest for max_integer()"""
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
+class Test_Max_Integers(unittest.TestCase):
+
+    def test_(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
 
 if __name__ == "__main__":
