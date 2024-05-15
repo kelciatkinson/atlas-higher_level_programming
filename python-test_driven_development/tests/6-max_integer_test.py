cdcc8a76399ca585315566ebe448ce396cbb209e
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 22:20:42 -0500

    added max at beginning test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,8 +7,11 @@ max_integer = __import__('6-max_integer').max_integer
 
 class Test_Max_Integers(unittest.TestCase):
 
-    def test_(self):
+    def max_at_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def max_at_beginning(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
 
 
 if __name__ == "__main__":
