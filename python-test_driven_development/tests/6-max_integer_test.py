9f423279206a4133f7821568c3ee6e6812e00186
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 22:22:25 -0500

    changed test function names

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,11 +7,12 @@ max_integer = __import__('6-max_integer').max_integer
 
 class Test_Max_Integers(unittest.TestCase):
 
-    def max_at_end(self):
+    def test_max_at_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def max_at_beginning(self):
+    def test_max_at_beginning(self):
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
 
 
 if __name__ == "__main__":
