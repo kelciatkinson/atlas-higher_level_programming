f5283c4bcf5117ebc0947adfa078cf5a46262725
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-14 21:57:16 -0500

    function prints lines of text split up by . ? or : and prints new lines after each

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this module contains a text indentation function"""
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    new_text = ""
+    space = False
+    punc = [".", "?", ":"]
+    for character in text:
+        if character in punc:
+            new_text += character + "\n\n"
+            space = True
+        elif character == " " and space is True:
+            pass
+        else:
+            new_text += character
+            space = False
+
+    for line in new_text.split("\n"):
+        print("".join(line.strip()))
