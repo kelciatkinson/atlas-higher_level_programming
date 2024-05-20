3af7921d4acc1358e76f0fd168b3a25c5cf923ae
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-20 11:34:28 -0500

    added documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module defines the function is_same_class"""
+
+
 def is_same_class(obj, a_class):
+    """This function checks if an object is
+    exactly an instance of the specified class"""
     if isinstance(obj, a_class):
         return True
     else:
