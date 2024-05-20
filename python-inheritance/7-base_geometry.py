888f143da9f1edf742e15f9b0daf97d926abc94e
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-20 13:38:47 -0500

    added to class with integer_validator function, no tests yet

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This module defines the BaseGeometry Class"""
+
+
+class BaseGeometry():
+    """initializes BaseGeometry"""
+    def area(self):
+        """This function raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates Value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
