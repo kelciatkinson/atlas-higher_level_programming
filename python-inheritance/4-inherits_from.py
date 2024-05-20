33b97ce34dba47739b1f7c39ebd67bed41e0e5b9
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-20 12:18:51 -0500

    function returning correct output

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""This module defines the inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """This function returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class;
+    otherwise False"""
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
+        return True
+    else:
+        return False
