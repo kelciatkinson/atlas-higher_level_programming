ff2354e44abb4173c9992638860bc577eebd2a66
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-20 13:27:44 -0500

    function to raise exception

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""This module defines the BaseGeometry Class"""
+
+
+class BaseGeometry():
+    """initializes BaseGeometry"""
+    def area(self):
+        """This function raises an Exception"""
+        raise Exception("area() is not implemented")
