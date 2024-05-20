3d4c403b6541f5b8fe8f907f508c646a73c6fc83
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-19 19:26:19 -0500

    modified doc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This module contains the class MyList and inherits from list"""
-
+"""This module contains the class MyList"""
 
 class MyList(list):
+    """This class inherits from list"""
     def __init__(self):
         """instance of MyList"""
         super().__init__()
