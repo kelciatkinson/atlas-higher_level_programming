449b2b348be4b76e8d8983826edbec0fd81f0c19
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-20 16:32:39 -0500

    changed file name to variable name

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 def read_file(filename=""):
     """This function reads a file"""
-    with open("my_file_0.txt", "r") as f:
+    with open(filename, "r") as f:
         f_contents = f.read()
         print(f_contents, end="")
