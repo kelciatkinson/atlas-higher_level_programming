dc5ba30e436b118240d3977acaa1b9602caef71f
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-19 19:24:59 -0500

    made class and print_sorted function

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""This module contains the class MyList and inherits from list"""
+
+
+class MyList(list):
+    def __init__(self):
+        """instance of MyList"""
+        super().__init__()
+
+    def print_sorted(self): 
+        """sorts and prints a given list"""
+        return list.sort(self)
