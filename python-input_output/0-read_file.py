20a53d9fc74db8bcff345f10964947a2b62fa1f0
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-20 16:28:28 -0500

    added doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""This module contains a function to read files"""
 
 def read_file(filename=""):
+    """This function reads a file"""
     with open("my_file_0.txt", "r") as f:
         f_contents = f.read()
         print(f_contents) 
