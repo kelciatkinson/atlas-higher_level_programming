238848964f811c78c298ce5b04b7e008d268dbe9
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 08:52:58 -0500

    rearranged methods

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -14,9 +14,8 @@ class Rectangle(BaseGeometry):
     def area(self):
         return self.__width * self.__height
 
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
     def print(self):
         print(self.__str__())
 
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
