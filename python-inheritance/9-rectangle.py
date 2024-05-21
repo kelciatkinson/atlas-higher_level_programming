384483efe5cc8c6157f517006c88c2e00a453215
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 08:45:25 -0500

    added area method

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This module defines the class Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This class creates a new object Rectangle"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
