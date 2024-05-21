80a952b08285b01e308260fce40f636804504287
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 18:47:07 -0500

    function to return diction description of an object

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Definition"""
+"""This module defines the class_to_json function"""
 
 def class_to_json(obj):
     """This function returns the dictionary description with 
     simple data structure (list, dictionary, string, integer
     and boolean) for JSON serialization of an object"""
+    return obj.__dict__
