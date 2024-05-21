0c328302d232376d9217ed9b99bf0dfff90f8be3
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 08:57:47 -0500

    added square class to print size

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This module defines the class Square"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """This class creates a new object Square"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
