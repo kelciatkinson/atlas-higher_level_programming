58ec1136d826019004f84d669c054a42a381fe87
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 09:18:33 -0500

    trying serialization check

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """This function writes an object to a text
     file, using a JSON representation"""
     with open(filename, "w") as file:
-        file.write(json.dumps(my_obj))
+        json.dump(my_obj, file)
