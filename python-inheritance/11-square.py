68be564cb9814e8c5c332e2c696697e05969a6bb
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 09:01:28 -0500

    change rectangle to square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This module defines the class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class creates a new object Square"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
