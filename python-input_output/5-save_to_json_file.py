2918941697a819de0c3dd6948dd507618d7053d0
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 10:20:27 -0500

    trying another function

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """This function writes an object to a text
     file, using a JSON representation"""
     with open(filename, "w") as file:
-        json.dump(my_obj, file)
+        json.dumps(my_obj, file)
---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines the load_from_json_file function"""
+import json
+
+
+def load_from_json_file(filename):
+    """This function writes an object to a text
+    file, using a JSON representation"""
