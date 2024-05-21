4ff0ea2a9aae51dc859925258282a91d7f7ffe39
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 09:23:56 -0500

    removed try/except

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,9 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """This function writes an object to a text
     file, using a JSON representation"""
-    try:
-        with open(filename, "w") as file:
-            json.dump(my_obj, file)
-    except Exception as e:
-        print("Error: {}".format(e))
-
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
