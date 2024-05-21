f92470658ce49fb5d8eca0c51410a9a884424a16
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-21 08:30:29 -0500

    added doc

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """This module defines the class Rectangle"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """This class creates a new object Rectangle"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
