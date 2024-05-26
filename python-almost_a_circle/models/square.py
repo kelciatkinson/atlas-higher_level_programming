978698dc8a44aa7ed15fde6fc0c848f160b5764f
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-26 18:07:47 -0500

    fixed line length

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,4 +11,5 @@ class Square(Rectangle):
     def __str__(self):
         """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return ("[{}] ({}) {}/{} - {}"
-                .format(self.__class__.__name__, self.id, self.x, self.y, self.width))
+                .format(self.__class__.__name__,
+                        self.id, self.x, self.y, self.width))
