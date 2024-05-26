3f6464d0dfa133d46866d6616ae8a743fad063c5
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-26 16:01:48 -0500

    updated Rectangle with __str__ override

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,7 @@ class Rectangle(Base):
                 print("#", end="")
             if i != self.__height:
                 print()
+
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
