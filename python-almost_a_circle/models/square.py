9eb74d5002a5f6b524590a8115c113786ad8ad3f
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-26 18:04:36 -0500

    added __str__ output

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,3 +7,8 @@ class Square(Rectangle):
     """Documentation will be ammended"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return ("[{}] ({}) {}/{} - {}"
+                .format(self.__class__.__name__, self.id, self.x, self.y, self.width))
