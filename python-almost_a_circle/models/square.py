db015cd56a019b543590ae22e98bc84249b9780e
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-26 20:16:57 -0500

    added getter/setter for size attribute of a square to assign width and height to equal value of size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,3 +13,17 @@ class Square(Rectangle):
         return ("[{}] ({}) {}/{} - {}"
                 .format(self.__class__.__name__,
                         self.id, self.x, self.y, self.width))
+
+    # getter for size
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
