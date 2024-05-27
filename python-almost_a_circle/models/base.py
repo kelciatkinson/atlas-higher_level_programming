fc377b3dc5268cdbd533db91a1b88133e452118c
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-27 14:56:05 -0500

    created classmethod for Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -44,3 +44,7 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """this method return an instance with all attributes already set"""
