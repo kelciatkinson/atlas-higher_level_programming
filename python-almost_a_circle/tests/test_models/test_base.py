668034c68d9926aa03453dcbef3db983c8408b31
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-28 15:49:15 -0500

    trying another assert to test an id of a Base instance

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,7 +8,8 @@ from models.base import Base
 class Test_Base(unittest.TestCase):
 
     def test_BaseID(self):
-        self.assertTrue(Base(id))
+        obj = Base()
+        self.assertIsNotNone(obj.id)
 
 if __name__ == "__main__":
     unittest.main()
