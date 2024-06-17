40b49cec6bd5ae4962d9659a614896fa516cb273
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-17 21:28:06 +0000

    added script to create user and grant privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- ! --
-CREATE USER user_0d_1 IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON localhost TO 'user_0d_1'@'localhost';
