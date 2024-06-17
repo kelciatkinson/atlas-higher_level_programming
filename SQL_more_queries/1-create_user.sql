95308f5206406e3569e3df758004bb2b5307abbd
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-17 21:49:55 +0000

    updated databse name

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- ! --
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON localhost TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
