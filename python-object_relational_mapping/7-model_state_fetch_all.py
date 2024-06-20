6277f198bde74f6cbead26cb1da7da17cd07f434
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 22:06:59 -0500

    whitespace

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
 
     session = Session()
