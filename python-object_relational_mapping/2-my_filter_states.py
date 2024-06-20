51990156ba76c714fb931dbf4c4c816fa3527e18
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-20 00:37:12 +0000

    trailing whitespace

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("""SELECT id, name 
+    cur.execute("""SELECT id, name
                 FROM states WHERE name = '{}'""".format(argv[4]))
 
     rows = cur.fetchall()
