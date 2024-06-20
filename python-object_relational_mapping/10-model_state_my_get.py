d911537229175b50eed9f45f1e884e1bcc60899d
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 23:06:58 -0500

    updated script

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -12,13 +12,14 @@ if __name__ == "__main__":
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
+    state_name = sys.argv[4]
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    states = session.query(State).filter(State.name == sys.argv[4]).first()
+    state = session.query(State).filter(State.name == state_name).first()
 
-    if states:
+    if state:
         print("{}".format(state.id))
     else:
         print("Not found")
