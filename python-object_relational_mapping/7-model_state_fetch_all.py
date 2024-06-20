792275e61d09d1767bcdeb428e99350d0f3e799d
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:58:17 -0500

    updated script

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     states = session.query(State).order_by(State.id).all()
 
-    for state in states():
+    for state in states:
         print("{}: {}".format(state.id, state.name))
 
     session.close()
