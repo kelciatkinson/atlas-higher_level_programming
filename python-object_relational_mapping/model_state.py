e65e090bc7b575737e4d3e0dc3b1d59f749722e3
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:53:23 -0500

    added script to list all states from database

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -18,7 +18,7 @@ class State(Base):
 
 if __name__ == "__main__":
     """Does not execute if imported"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
