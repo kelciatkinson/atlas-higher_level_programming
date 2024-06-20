9c93513dd5026c86dcdc646a8081948b04697a62
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-20 00:07:14 +0000

    added script to show states starting with uppercase n

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Here is some documentation"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """This is so the file will not be executed when imported"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT DISTINCT id, name FROM states WHERE name LIKE 'N%';")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
