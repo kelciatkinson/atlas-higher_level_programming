26b4e9cf8a0a90262e0333e779b4bac3e6674362
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:42:38 -0500

    whitespace

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,9 +9,11 @@ Base = declarative_base()
 
 class State(Base):
     """Class State inherits from Base"""
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
     """Does not execute if imported"""
