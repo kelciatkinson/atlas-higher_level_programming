0e4025621b8d475b3deef19961ad90abff7c47b4
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:16:03 -0500

    updated script

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = """SELECT cities.name FROM cities JOIN states
+    query = """SELECT cities.name FROM states JOIN cities
     ON cities.state_id = states.id WHERE states.name = %s"""
     cur.execute(query, (argv[4],))
 
