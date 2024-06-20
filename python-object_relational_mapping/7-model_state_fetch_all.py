35b1ebaa4f88c5444cbfd37cb07f68c98c820b37
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:57:36 -0500

    updated script

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """Does not execute if imported"""
-    engine = create_engine('mysql://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     
     Session = sessionmaker(bind=engine)
