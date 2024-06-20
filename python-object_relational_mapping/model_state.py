42004200d53dfed51eb5073e325697968a709547
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:37:28 -0500

    added documentation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,11 +9,13 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class State(Base):
+    """Class State inherits from Base"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
+    """Does not execute if imported"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
