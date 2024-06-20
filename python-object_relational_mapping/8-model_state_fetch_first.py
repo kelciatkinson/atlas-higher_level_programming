224b5b709af0e3f16ccff9c622e18f0b88798b56
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 22:26:52 -0500

    updated script

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,13 +11,12 @@ if __name__ == "__main__":
     """Does not execute if imported"""
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    state = session.query(State).order_by(State.id).all()
+    state = session.query(State).order_by(State.id).first()
 
     if state:
         print("{}: {}".format(state.id, state.name))
