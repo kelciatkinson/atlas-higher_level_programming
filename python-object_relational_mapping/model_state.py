3b2874a0f0d6db5ab375e2f7263d47f478689937
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:38:15 -0500

    updated imports

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Here is documentation"""
 import sys
-from model_state import Base, State
-from sqlalchemy import create_engine, declarative_base
+from SQLAlchemy import create_engine, declarative_base
 from sqlalchemy import Column, Integer, String
 
 
