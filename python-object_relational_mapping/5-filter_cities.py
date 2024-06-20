494f3f0c8031928fceab6199b4671d6da3611b42
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-19 21:14:44 -0500

    added script to list all cities of a given state from the database

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Here is some documentation"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """This is so the file will not be executed when imported"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+
+    cur = db.cursor()
+
+    query = """SELECT cities.name FROM cities JOIN states
+    ON cities.state_id = states.id WHERE states.name = %s"""
+    cur.execute(query, (argv[4],))
+
+    rows = cur.fetchall()
+
+    cities = []
+
+    for row in rows:
+        cities.append(row[0])
+
+    if cities:
+        print("".join(cities))
+
+    cur.close()
+    db.close()
+
