43f31f10390754a5b760f9a68bbd7f544968d87a
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-18 15:18:13 -0500

    task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// print process.argv
+if (process.argv[2] == null) {
+  console.log('No argument');
+} else if (process.argv[3] == null) {
+  console.log('Argument found');
+} else { console.log('Arguments found'); }
