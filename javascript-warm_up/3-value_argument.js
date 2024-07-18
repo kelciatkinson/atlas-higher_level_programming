c6615d6351c05ddb766dda1d6cf7cbe17ba8fa8e
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-18 15:37:17 -0500

    semistandard

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/local/bin/node
 // print 1st argument
 if (process.argv[2] == null) {
-    console.log('No argument');
-  } else if (process.argv[3] == null) {
-    console.log(process.argv[2]);
-  } else { console.log(process.argv[2]); }
-  
+  console.log('No argument');
+} else if (process.argv[3] == null) {
+  console.log(process.argv[2]);
+} else { console.log(process.argv[2]); }
