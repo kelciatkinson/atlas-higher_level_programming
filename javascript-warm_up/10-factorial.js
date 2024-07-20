f5a30d3ff86aab8df712a20a47eb25a6013d3832
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-20 14:41:52 -0500

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (a <= 0) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+
+const num = Number(process.argv[2]);
+
+console.log(factorial(num));
