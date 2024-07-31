9d3b87a205348952d00acddeb26a08a0a980d3f4
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-30 21:43:14 -0500

    semistandard

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,25 +5,25 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
-        process.exit(1);
-    }
-    
-    if (response.statusCode === 200) {
-        const todos = JSON.parse(body);
-        const completedTasks = {};
+  if (error) {
+    console.error('error:', error);
+    process.exit(1);
+  }
 
-        todos.forEach(todo => {
-            if (todo.completed) {
-                if (!completedTasks[todo.userId]) {
-                    completedTasks[todo.userId] = 0;
-                }
-                completedTasks[todo.userId]++;
-            }
-        });
-        console.log(completedTasks);
-    } else {
-        process.exit(1);
-    }
+  if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (!completedTasks[todo.userId]) {
+          completedTasks[todo.userId] = 0;
+        }
+        completedTasks[todo.userId]++;
+      }
+    });
+    console.log(completedTasks);
+  } else {
+    process.exit(1);
+  }
 });
